b5ce455fcddd7f368e300cbe789644b60391369f
Chaerin-Annisa <annisachaerin0@gmail.com>
2024-06-22 17:38:13 +0700

    Update animal.php

--- day9/animal.php
+++ day9/animal.php
@@ -2,8 +2,6 @@
 class animal{
     public $legs =4;
     public $cold_blooded ="no";
-    public $yell="auooo";
-    public $jump="hop-hop";
     public $name;
     public function __construct($name){
      $this->name=$name;     
